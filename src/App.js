dd2def33ac6f8d6fd587e6770e2987fa0e759675
DeQuan Sanders <dequansanders17@gmail.com>
2021-11-19 21:29:47 -0600

    attempt2
    trying to get my content on the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Particles from "react-tsparticles";
+import  Navigation  from "./components/Navigation/Navigation";
 import './App.css';
+import particlesOptions from "./particles.json";
 
 function App() {
-  
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      
-    </main>
-  );
+    return (
+        <div className="App">
+            <Particles options={particlesOptions}/>
+            <Navigation />
+                
+            
+        </div>
+    );
 }
 
 export default App;
