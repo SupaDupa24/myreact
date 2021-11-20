e9141ebe23b2e285c172249819a469d5225deea7
DeQuan Sanders <dequansanders17@gmail.com>
2021-11-19 21:32:55 -0600

    attempt3
    remove particles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Particles from "react-tsparticles";
 import  Navigation  from "./components/Navigation/Navigation";
 import './App.css';
-import particlesOptions from "./particles.json";
+//import particlesOptions from "./particles.json";
 
 function App() {
     return (
         <div className="App">
-            <Particles options={particlesOptions}/>
+            {/* <Particles options={particlesOptions}/>  */}
             <Navigation />
                 
             
